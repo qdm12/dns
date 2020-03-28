6c70c2eff3b5f7b4cb1727dc7e508478c7b92f6d
Florian Stosse <Harvester57@users.noreply.github.com>
2020-03-28 20:45:03 +0100

    Fix caching logic (#32) - Harvester57

--- internal/dns/conf.go
+++ internal/dns/conf.go
@@ -103,9 +103,9 @@ func generateUnboundConf(settings models.Settings, client network.Client, logger
 		"forward-tls-upstream": "yes",
 	}
 	if settings.Caching {
-		forwardZoneSection["forward-no-cache"] = "yes"
-	} else {
 		forwardZoneSection["forward-no-cache"] = "no"
+	} else {
+		forwardZoneSection["forward-no-cache"] = "yes"
 	}
 	var forwardZoneLines []string
 	for k, v := range forwardZoneSection {
