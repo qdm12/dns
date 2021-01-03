9c3e47cadbf5586820e760e1c6ebe2da9d066a5c
Quentin McGaw <quentin.mcgaw@gmail.com>
2021-01-03 03:50:16 +0000

    Fix UseDNSSystemWide

--- pkg/unbound/nameserver.go
+++ pkg/unbound/nameserver.go
@@ -19,8 +19,7 @@ func (c *configurator) UseDNSInternally(ip net.IP) {
 
 // UseDNSSystemWide changes the nameserver to use for DNS system wide.
 func (c *configurator) UseDNSSystemWide(ip net.IP, keepNameserver bool) error {
-	const filepath = resolvConfFilepath
-	file, err := c.openFile(filepath, os.O_RDWR|os.O_TRUNC, 0644)
+	file, err := c.openFile(resolvConfFilepath, os.O_RDWR|os.O_TRUNC, 0644)
 	if err != nil {
 		return err
 	}
@@ -29,23 +28,20 @@ func (c *configurator) UseDNSSystemWide(ip net.IP, keepNameserver bool) error {
 		_ = file.Close()
 		return err
 	}
+
 	s := strings.TrimSuffix(string(data), "\n")
-	lines := strings.Split(s, "\n")
-	if len(lines) == 1 && lines[0] == "" {
-		lines = nil
+
+	lines := []string{
+		"nameserver " + ip.String(),
 	}
-	found := false
-	if !keepNameserver { // default
-		for i := range lines {
-			if strings.HasPrefix(lines[i], "nameserver ") {
-				lines[i] = "nameserver " + ip.String()
-				found = true
-			}
+	for _, line := range strings.Split(s, "\n") {
+		if line == "" ||
+			(!keepNameserver && strings.HasPrefix(line, "nameserver ")) {
+			continue
 		}
+		lines = append(lines, line)
 	}
-	if !found {
-		lines = append(lines, "nameserver "+ip.String())
-	}
+
 	s = strings.Join(lines, "\n") + "\n"
 	_, err = file.WriteString(s)
 	if err != nil {
