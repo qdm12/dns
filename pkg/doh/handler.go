0b03ce88ed7712e5cb01730ede8ade74841d0808
Quentin McGaw <quentin.mcgaw@gmail.com>
2021-03-23 02:33:55 +0000

    Using faster random generator

--- pkg/doh/handler.go
+++ pkg/doh/handler.go
@@ -3,11 +3,11 @@ package doh
 import (
 	"context"
 	"sync"
-	"sync/atomic"
 
 	"github.com/miekg/dns"
 	"github.com/qdm12/dns/pkg/dot"
 	"github.com/qdm12/dns/pkg/provider"
+	"github.com/qdm12/golibs/crypto/random/hashmap"
 	"github.com/qdm12/golibs/logging"
 )
 
@@ -22,7 +22,7 @@ type handler struct {
 	// Internal objects
 	udpBufferPool *sync.Pool
 	httpHandler   HTTPHandler
-	counter       *int64
+	rand          hashmap.Rand
 }
 
 func newDNSHandler(ctx context.Context, logger logging.Logger,
@@ -53,7 +53,7 @@ func newDNSHandler(ctx context.Context, logger logging.Logger,
 		logger:        logger,
 		udpBufferPool: udpBufferPool,
 		httpHandler:   newHTTPHandler(dotOptions...),
-		counter:       new(int64),
+		rand:          hashmap.New(),
 	}
 }
 
@@ -69,8 +69,7 @@ func (h *handler) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
 	}
 
 	// Pick DoH url pseudo-randomly from the chosen providers
-	i := int(atomic.AddInt64(h.counter, 1))
-	i %= len(h.dohServers)
+	i := h.rand.Intn(len(h.dohServers))
 	dohServer := h.dohServers[i]
 
 	respWire, err := h.httpHandler.Request(h.ctx, dohServer.URL, wire)
--- pkg/dot/picker.go
+++ pkg/dot/picker.go
@@ -2,82 +2,67 @@ package dot
 
 import (
 	"net"
-	"sync/atomic"
 
 	"github.com/qdm12/dns/pkg/provider"
+	"github.com/qdm12/golibs/crypto/random/hashmap"
 )
 
 type picker struct {
-	counterDoTServer *int64
-	counterDoTIPv4   *int64
-	counterDoTIPv6   *int64
-	counterDNSServer *int64
-	counterDNSIPv4   *int64
-	counterDNSIPv6   *int64
+	rand hashmap.Rand
 }
 
 func newPicker() *picker {
 	return &picker{
-		counterDoTServer: new(int64),
-		counterDoTIPv4:   new(int64),
-		counterDoTIPv6:   new(int64),
-		counterDNSServer: new(int64),
-		counterDNSIPv4:   new(int64),
-		counterDNSIPv6:   new(int64),
+		rand: hashmap.New(),
 	}
-}
-
-func (p *picker) randIndex(counter *int64, max int) int {
-	index := int(atomic.AddInt64(counter, 1))
-	return index % max
 }
 
 func (p *picker) DoTServer(servers []provider.DoTServer) provider.DoTServer {
 	index := 0
 	if nServers := len(servers); nServers > 1 {
-		index = p.randIndex(p.counterDoTServer, nServers)
+		index = p.rand.Intn(nServers)
 	}
 	return servers[index]
 }
 
-func (p *picker) IP(counter *int64, ips []net.IP) net.IP {
+func (p *picker) IP(ips []net.IP) net.IP {
 	switch len(ips) {
 	case 0:
 		return nil
 	case 1:
 		return ips[0]
 	default:
-		index := p.randIndex(counter, len(ips))
+		index := p.rand.Intn(len(ips))
 		return ips[index]
 	}
 }
 
 func (p *picker) DoTIP(server provider.DoTServer, ipv6 bool) net.IP {
 	if ipv6 {
-		if ip := p.IP(p.counterDoTIPv6, server.IPv6); ip != nil {
+		if ip := p.IP(server.IPv6); ip != nil {
 			return ip
 		}
 		// if there is no IPv6, fall back to an IPv4 address
 		// as all provider have at least an IPv4 address.
 	}
-	return p.IP(p.counterDoTIPv4, server.IPv4)
+	return p.IP(server.IPv4)
 }
 
 func (p *picker) DNSServer(servers []provider.DNSServer) provider.DNSServer {
 	index := 0
 	if nServers := len(servers); nServers > 1 {
-		index = p.randIndex(p.counterDNSServer, nServers)
+		index = p.rand.Intn(nServers)
 	}
 	return servers[index]
 }
 
 func (p *picker) DNSIP(server provider.DNSServer, ipv6 bool) net.IP {
 	if ipv6 {
-		if ip := p.IP(p.counterDNSIPv6, server.IPv6); ip != nil {
+		if ip := p.IP(server.IPv6); ip != nil {
 			return ip
 		}
 		// if there is no IPv6, fall back to an IPv4 address
 		// as all provider have at least an IPv4 address.
 	}
-	return p.IP(p.counterDNSIPv4, server.IPv4)
+	return p.IP(server.IPv4)
 }
