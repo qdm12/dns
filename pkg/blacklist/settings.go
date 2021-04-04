020726e7a61a1ce81ac325bccd396738beca93b0
Quentin McGaw <quentin.mcgaw@gmail.com>
2021-04-04 00:33:03 +0000

    Feature: blacklist settings AddBlockHostnames

--- pkg/blacklist/settings.go
+++ pkg/blacklist/settings.go
@@ -2,6 +2,7 @@ package blacklist
 
 import (
 	"net"
+	"sort"
 	"strconv"
 	"strings"
 
@@ -14,11 +15,37 @@ type Settings struct {
 	IPNets        []*net.IPNet
 }
 
+// AddBlockHostnames transforms the slice of hostnames given to
+// FQDN hostnames and sets these to the settings.
 func (s *Settings) BlockHostnames(hostnames []string) {
 	s.FqdnHostnames = make([]string, len(hostnames))
 	for i := range hostnames {
 		s.FqdnHostnames[i] = dns.Fqdn(hostnames[i])
 	}
+}
+
+// AddBlockHostnames transforms the slice of hostnames given to
+// FQDN hostnames and adds the new hostnames to the settings,
+// removing any duplicate.
+func (s *Settings) AddBlockHostnames(hostnames []string) {
+	fqdnHostnames := make([]string, len(hostnames))
+	for i := range hostnames {
+		fqdnHostnames[i] = dns.Fqdn(hostnames[i])
+	}
+	set := make(map[string]struct{}, len(s.FqdnHostnames)+len(fqdnHostnames))
+	for _, host := range s.FqdnHostnames {
+		set[host] = struct{}{}
+	}
+	for _, host := range fqdnHostnames {
+		set[host] = struct{}{}
+	}
+	s.FqdnHostnames = make([]string, 0, len(set))
+	for host := range set {
+		s.FqdnHostnames = append(s.FqdnHostnames, host)
+	}
+	sort.Slice(s.FqdnHostnames, func(i, j int) bool {
+		return s.FqdnHostnames[i] < s.FqdnHostnames[j]
+	})
 }
 
 func (s *Settings) String() string {
---
+++ pkg/blacklist/settings_test.go
@@ -0,0 +1,95 @@
+package blacklist
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Settings_BlockHostnames(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		initialSettings Settings
+		hostnames       []string
+		finalSettings   Settings
+	}{
+		"nothing": {
+			finalSettings: Settings{
+				FqdnHostnames: []string{},
+			},
+		},
+		"insert new first ones": {
+			hostnames: []string{"abc.com", "def.co.uk"},
+			finalSettings: Settings{
+				FqdnHostnames: []string{"abc.com.", "def.co.uk."},
+			},
+		},
+		"override": {
+			initialSettings: Settings{
+				FqdnHostnames: []string{"01.com.", "abc.com."},
+			},
+			hostnames: []string{"abc.com", "def.co.uk"},
+			finalSettings: Settings{
+				FqdnHostnames: []string{"abc.com.", "def.co.uk."},
+			},
+		},
+	}
+
+	for name, testCase := range testCases {
+		testCase := testCase
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			settings := testCase.initialSettings
+
+			settings.BlockHostnames(testCase.hostnames)
+
+			assert.Equal(t, testCase.finalSettings, settings)
+		})
+	}
+}
+
+func Test_Settings_AddBlockHostnames(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		initialSettings Settings
+		hostnames       []string
+		finalSettings   Settings
+	}{
+		"nothing": {
+			finalSettings: Settings{
+				FqdnHostnames: []string{},
+			},
+		},
+		"insert new first ones": {
+			hostnames: []string{"abc.com", "def.co.uk"},
+			finalSettings: Settings{
+				FqdnHostnames: []string{"abc.com.", "def.co.uk."},
+			},
+		},
+		"insert two with one duplicate": {
+			initialSettings: Settings{
+				FqdnHostnames: []string{"01.com.", "abc.com."},
+			},
+			hostnames: []string{"abc.com", "def.co.uk"},
+			finalSettings: Settings{
+				FqdnHostnames: []string{"01.com.", "abc.com.", "def.co.uk."},
+			},
+		},
+	}
+
+	for name, testCase := range testCases {
+		testCase := testCase
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			settings := testCase.initialSettings
+
+			settings.AddBlockHostnames(testCase.hostnames)
+
+			assert.Equal(t, testCase.finalSettings, settings)
+		})
+	}
+}
