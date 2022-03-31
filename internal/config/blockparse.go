632c30799b0ff7d5a0ac454ae386648ed9a268ce
Quentin McGaw <quentin.mcgaw@gmail.com>
2022-03-31 07:43:48 +0000

    Fix parsing of IPs and IP prefixes - For `BLOCK_IPS` - For `ALLOWED_IPS` - For `BLOCK_CIDRS` - For `ALLOWED_CIDRS`

--- internal/config/blockparse.go
+++ internal/config/blockparse.go
@@ -24,12 +24,11 @@ var errIPStringInvalid = errors.New("IP address string is invalid")
 func parseIPStrings(ipStrings []string) (ips []netaddr.IP, err error) {
 	ips = make([]netaddr.IP, len(ipStrings))
 
-	for _, ipString := range ipStrings {
-		ip, err := netaddr.ParseIP(ipString)
+	for i, ipString := range ipStrings {
+		ips[i], err = netaddr.ParseIP(ipString)
 		if err != nil {
 			return nil, fmt.Errorf("%w: %s", errIPStringInvalid, err)
 		}
-		ips = append(ips, ip)
 	}
 
 	return ips, nil
@@ -40,12 +39,11 @@ var errIPPrefixStringInvalid = errors.New("IP prefix CIDR string is invalid")
 func parseIPPrefixStrings(ipPrefixStrings []string) (ipPrefixes []netaddr.IPPrefix, err error) {
 	ipPrefixes = make([]netaddr.IPPrefix, len(ipPrefixStrings))
 
-	for _, ipPrefixString := range ipPrefixStrings {
-		ipPrefix, err := netaddr.ParseIPPrefix(ipPrefixString)
+	for i, ipPrefixString := range ipPrefixStrings {
+		ipPrefixes[i], err = netaddr.ParseIPPrefix(ipPrefixString)
 		if err != nil {
 			return nil, fmt.Errorf("%w: %s", errIPPrefixStringInvalid, err)
 		}
-		ipPrefixes = append(ipPrefixes, ipPrefix)
 	}
 
 	return ipPrefixes, nil
