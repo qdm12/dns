2a1b01844832b8da0cf836a802e04d46e272ce2e
Quentin McGaw <quentin.mcgaw@gmail.com>
2023-11-24 06:32:07 +0000

    feat(provider): add libredns DoT IPv6 address

--- pkg/provider/libredns.go
+++ pkg/provider/libredns.go
@@ -21,7 +21,9 @@ func LibreDNS() Provider {
 			IPv4: []netip.AddrPort{
 				defaultDoTIPv4AddrPort([4]byte{116, 202, 176, 26}),
 			},
-			IPv6: []netip.AddrPort{},
+			IPv6: []netip.AddrPort{
+				defaultDoTIPv6AddrPort([16]byte{0x2a, 0x1, 0x4, 0xf8, 0x1c, 0xc, 0x82, 0x74, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1}),
+			},
 			Name: "dot.libredns.gr",
 		},
 		// see https://libredns.gr/
