546e124be56c1159ef3bfb96486c3a4dac880fee
Quentin McGaw <quentin.mcgaw@gmail.com>
2023-11-24 07:39:08 +0000

    fix(provider): remove Quadrant IPv6 bad address

--- pkg/provider/quadrant.go
+++ pkg/provider/quadrant.go
@@ -9,20 +9,17 @@ func Quadrant() Provider {
 			IPv4: []netip.AddrPort{
 				defaultDNSIPv4AddrPort([4]byte{12, 159, 2, 159}),
 			},
-			IPv6: []netip.AddrPort{
-				defaultDNSIPv6AddrPort([16]byte{0x20, 0x1, 0x18, 0x90, 0x14, 0xc, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0x59}),
-			},
+			IPv6: []netip.AddrPort{},
 		},
+		// See https://quadrantsec.com/blog/quadrants-public-dns-resolver-tls-https-support
 		DoT: DoTServer{
 			IPv4: []netip.AddrPort{
 				defaultDoTIPv4AddrPort([4]byte{12, 159, 2, 159}),
 			},
-			IPv6: []netip.AddrPort{
-				defaultDoTIPv6AddrPort([16]byte{0x20, 0x1, 0x18, 0x90, 0x14, 0xc, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0x59}),
-			},
+			IPv6: []netip.AddrPort{},
 			Name: "dns-tls.qis.io",
 		},
-		// See https://quadrantsec.com/quadrants_public_dns_resolver_with_tls_https_support/
+		// See https://quadrantsec.com/blog/quadrants-public-dns-resolver-tls-https-support
 		DoH: DoHServer{
 			URL: "https://doh.qis.io/dns-query",
 		},
