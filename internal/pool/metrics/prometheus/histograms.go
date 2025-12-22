dfa1dff07c03d99271d746140635f6bb81d30e08
Quentin McGaw <quentin.mcgaw@gmail.com>
2025-12-22 17:07:00 +0000

    Specify bucket upper bounds for histograms

--- internal/pool/metrics/prometheus/histograms.go
+++ internal/pool/metrics/prometheus/histograms.go
@@ -17,9 +17,31 @@ func newHistograms(settings prom.Settings) (h *histograms, err error) {
 	prefix := settings.Prefix
 	h = &histograms{
 		useTimes: helpers.NewHistogramVec(prefix, "pool_connection_usetime",
-			"Pool connections in use duration spent by address", []string{"address"}, nil),
+			"Pool connections in use duration in seconds spent by address", []string{"address"},
+			[]float64{
+				time.Millisecond.Seconds(),
+				10 * time.Millisecond.Seconds(),
+				50 * time.Millisecond.Seconds(),
+				100 * time.Millisecond.Seconds(),
+				300 * time.Millisecond.Seconds(),
+				500 * time.Millisecond.Seconds(),
+				time.Second.Seconds(),
+			}),
 		lifetimes: helpers.NewHistogramVec(prefix, "pool_connection_lifetime",
-			"Pool connection total lifetime duration in seconds by address", []string{"address"}, nil),
+			"Pool connection total lifetime duration in seconds by address", []string{"address"},
+			[]float64{
+				time.Second.Seconds(),
+				10 * time.Second.Seconds(),
+				20 * time.Second.Seconds(),
+				30 * time.Second.Seconds(),
+				time.Minute.Seconds(),
+				2 * time.Minute.Seconds(),
+				5 * time.Minute.Seconds(),
+				15 * time.Minute.Seconds(),
+				30 * time.Minute.Seconds(),
+				time.Hour.Seconds(),
+				2 * time.Hour.Seconds(),
+			}),
 	}
 	err = helpers.Register(settings.Registry, h.useTimes, h.lifetimes)
 	if err != nil {
